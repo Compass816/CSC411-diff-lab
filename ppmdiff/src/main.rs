54d4ac107f7429bd0ed08ea50d5a1620a1f2a9ad
Apps <123339291+ry-bro-uri@users.noreply.github.com>
2023-11-07 22:19:33 -0500

    Update main.rs

--- ppmdiff/src/main.rs
+++ ppmdiff/src/main.rs
@@ -1,17 +1,14 @@
 use array2::Array2;
-use csc411_image::{RgbImage, Read, Rgb};
-use std::env;
+use csc411_image::{Read, Rgb, RgbImage};
 use std::convert::TryInto;
+use std::env;
 
 fn main() {
     let ppm1 = env::args().nth(1);
     let ppm2 = env::args().nth(2);
 
     // assert only two arguments are given in the command line
-    assert!(
-        env::args().len() <= 3,
-        "Too many arguments!"
-    );
+    assert!(env::args().len() <= 3, "Too many arguments!");
 
     // Set rgb image with th filename
     let img1 = RgbImage::read(ppm1.as_deref()).unwrap();
@@ -27,25 +24,46 @@ fn main() {
     let pixels_vec1: Vec<Rgb> = img1.pixels.clone();
     let pixels_vec2: Vec<Rgb> = img2.pixels.clone();
 
-    // Create Array2s 
+    // Create Array2s
     let img1 = Array2::from_row_major(width1, height1, pixels_vec1).unwrap();
     let img2 = Array2::from_row_major(width2, height2, pixels_vec2).unwrap();
 
-     let mut e = 0.0;
-    for (x, y, pixel1) in img1.iter_row_major() {
-        let pixel2 = img2.get(x, y);
-
-        let r1 = pixel1.red as f64;
-        let g1 = pixel1.green as f64;
-        let b1 = pixel1.blue as f64;
-
-        let r2 = pixel2.red as f64;
-        let g2 = pixel2.green as f64;
-        let b2 = pixel2.blue as f64;
-
-        e += (r1 - r2).powi(2) + (g1 - g2).powi(2) + (b1 - b2).powi(2);
+    // Make sure widths and heights only different by at most 1
+    if width1.abs_diff(width2) > 1 && height1.abs_diff(height2) > 1 {
+        eprintln!("Width or height differs by more than 1");
+        println!("1.0");
     }
-    e = e.sqrt();
-    e = e / (3.0 * width1 as f64 * height1 as f64);
-    println!("{:.4}", e);
+
+    else {
+        // Compute the RMSD
+        let mut e = 0.0;
+        for (x, y, pixel1) in img1.iter_row_major() {
+            let pixel2 = img2.get(x, y);
+
+            let r1 = pixel1.red as f64;
+            let g1 = pixel1.green as f64;
+            let b1 = pixel1.blue as f64;
+
+            let r2 = pixel2.red as f64;
+            let g2 = pixel2.green as f64;
+            let b2 = pixel2.blue as f64;
+
+            e += ((r1 - r2).powi(2)) + ((g1 - g2).powi(2)) + ((b1 - b2).powi(2));
+        }
+
+        let mut small_w = width1 as f64;
+        let mut small_h = height1 as f64;
+
+        if width2 < width1 {
+            small_w = width2 as f64;
+        }
+        if height2 < height1 {
+            small_h = height2 as f64;
+        }
+
+        let msd = e / (3.0 * small_w * small_h);
+        let rmsd = msd.sqrt();
+
+        println!("{:.4}", rmsd);
+    }
 }
